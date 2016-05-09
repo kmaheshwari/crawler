a4e983613614d3fb7565413c017defb97ced33b1
kmaheshwari <a.thakur73.at@gmail.com>
2016-05-09 17:50:21 +0530

    some changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,21 @@ class DashboardController < ApplicationController
 	require 'rubygems'
 	require 'nokogiri'   
 	require 'open-uri'
-	#require 'market_bot'
+	
 	def index
-		url = "https://play.google.com/store/apps/category/LIFESTYLE"
-		@page = Nokogiri::HTML(open(url))
+		@all=[]
+		url = "https://play.google.com/store/apps"
+		@page = Nokogiri(open(url))
 		@data = @page.css("a.see-more")
-		#@test=
+		@data.each do |d|
+			@all.append(d['href'])
+		end
+		app_info=[]
+		@all.each do |n|
+			@u= "https://play.google.com"+n
+			@page1 = Nokogiri(open(url))
+			#@app_info.append(@page1.css("div.id"app-title"))
+		end
 	end
 	def show
 	end
