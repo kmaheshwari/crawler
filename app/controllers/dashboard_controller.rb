328f9f2a98512561871359bff52d1fd8f8cc6a56
rajiv-shrivastava <rjv834@gmail.com>
2016-05-10 15:58:04 +0530

    excel working

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,14 +4,43 @@ class DashboardController < ApplicationController
 	require 'open-uri'
 
 	def index
+
+                  
+
 	end
 
 
 	def scrap
-		#url =  params[:cateogry].to_s
-		url = "https://play.google.com/store/apps"
+
+
+        @all_emails = [] 
+
+        @all_apps = [] 
+
+		url =  params[:cateogry].to_s
 		@page = Nokogiri::HTML(open(url))
-		#@data = @page.css("a.dev-link")[0]["href"]
-		@data1 = @page.css("a.see-more")
-	end
+
+		@page.css("div.details").each do |item| 
+	 
+     	 app_name =  item.css("a").text 
+	     @all_apps.push(app_name) 
+          
+         url_developer =  item.css("a")[1]["href"] 
+
+	     url2 = "https://play.google.com#{url_developer}"  
+	
+
+		 email = Nokogiri::HTML(open(url2)) 
+	     email_value =  email.css("a.dev-link")[1].to_s 
+	     
+	     index_chop = email_value.index(">").to_i      
+		 email_value[index_chop+6..-5] 
+		 @all_emails.push(email_value[index_chop+6..-5])
+
+          end
+
+
+end
+
+
 end
