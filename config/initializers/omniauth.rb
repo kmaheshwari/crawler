9012f3f66a4073046392d10e6a630cd5adeb4be9
kmaheshwari <a.thakur73.at@gmail.com>
2016-05-10 16:59:59 +0530

    remove google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OmniAuth.config.logger = Rails.logger
+# OmniAuth.config.logger = Rails.logger
 
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '576899298088-cldbq2ecbl7nm84m8orfiukuidf3p121.apps.googleusercontent.com', '1VtgQRGHsOIKEm24LVKG79yK'
-end
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :google_oauth2, '576899298088-cldbq2ecbl7nm84m8orfiukuidf3p121.apps.googleusercontent.com', '1VtgQRGHsOIKEm24LVKG79yK'
+# end
